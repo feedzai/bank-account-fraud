9abd21ca703607f6bb6ea55f20bbe9c4e70a1dda
sergio.jesus <sergio.jesus@feedzai.com>
2022-06-16 02:28:56 +0100

    Add documents and reproducibility notebooks. Create readme.

---
+++ notebooks/mvn.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import warnings
+
+from abc import ABC, abstractmethod
+from scipy.stats import multivariate_normal as mvn
+from scipy.optimize import fsolve
+from typing import Any, Callable, Tuple
+
+class TypeIIIBiasSampler():
+    def __init__(
+        self,
+        label_col: str,
+        protected_attribute: str,
+        recall_first_group: float,
+        recall_second_group: float,
+        fpr_first_group: float = 0.05,
+        fpr_second_group: float = 0.05,
+        protected_attribute_values: Tuple[str, str] = None,
+        seed: int = 42,
+        feature_names: Tuple[str, str] = ("x1", "x2"),
+    ):
+        self.label_col = label_col
+        self.protected_attribute = protected_attribute
+        self.recall_first_group = recall_first_group
+        self.recall_second_group = recall_second_group
+        self.fpr_first_group = fpr_first_group
+        self.fpr_second_group = fpr_second_group
+        self.pro_attr_values = protected_attribute_values
+        self.seed = seed
+        self.feature_names = feature_names
+        self.mvn_neg, self.mvn_group_1, self.mvn_group_2 = (
+            self._calculate_multivariate_normals()
+        )
+
+    def __call__(self, data: pd.DataFrame, inplace: bool = False) -> pd.DataFrame:
+        if not inplace:
+            data = data.copy()
+        if not self.pro_attr_values:
+            self.pro_attr_values = data[self.protected_attribute].unique()
+            warnings.warn(
+                f"Protected attribute values order not passed. Using {self.pro_attr_values}.",
+                UserWarning,
+            )
+
+        # Add new columns to dataframe
+        data[self.feature_names] = 1
+
+        # Conditions for filtering dataframe.
+        a_pos = ((data[self.protected_attribute] == self.pro_attr_values[0]) &
+                 (data[self.label_col] == 1))
+        b_pos = ((data[self.protected_attribute] == self.pro_attr_values[1]) &
+                 (data[self.label_col] == 1))
+        a_neg = ((data[self.protected_attribute] == self.pro_attr_values[0]) &
+                 (data[self.label_col] == 0))
+        b_neg = ((data[self.protected_attribute] == self.pro_attr_values[1]) &
+                 (data[self.label_col] == 0))
+
+        data.loc[a_pos, self.feature_names] = self.mvn_group_1.rvs(data[a_pos].shape[0])
+        data.loc[b_pos, self.feature_names] = self.mvn_group_2.rvs(data[b_pos].shape[0])
+        data.loc[a_neg, self.feature_names] = self.mvn_neg.rvs(data[a_neg].shape[0])
+        data.loc[b_neg, self.feature_names] = self.mvn_neg.rvs(data[b_neg].shape[0])
+
+        return data
+
+    def _calculate_multivariate_normals(self) -> Tuple[mvn, mvn, mvn]:
+        def get_mean(var: Tuple[float, float]) -> Tuple[float, float]:
+            intercept, new_mean = var
+            new_dist = mvn(mean=[new_mean, 0], cov=cov_matrix)
+
+            obj_1 = 1 - mvn_negative.cdf([intercept, 0]) * 2 - fpr
+            obj_2 = 1 - new_dist.cdf([intercept, 0]) * 2 - recall
+            return obj_1, obj_2
+
+        cov_matrix = [[1, 0], [0, 1]]
+        mvn_negative = mvn(mean=[0, 0], cov=cov_matrix, seed=self.seed)
+
+        fpr_list = [self.fpr_first_group, self.fpr_second_group]
+        recall_list = [self.recall_first_group, self.recall_second_group]
+
+        distributions = []
+        for fpr, recall in zip(fpr_list, recall_list):
+            estimate = [1.0, 1.0]
+            _, mean = fsolve(get_mean, estimate)
+
+            np.random.seed(self.seed)
+            theta = 0.25 * np.pi
+            rotation_matrix = np.array(
+                [[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]
+            )
+            mean = np.array([mean, 0])
+            rotated_mean = np.matmul(rotation_matrix, mean)
+            distributions.append(mvn(mean=rotated_mean, cov=cov_matrix, seed=self.seed))
+
+        return mvn_negative, distributions[0], distributions[1]
+    
---
+++ notebooks/random_search.py
@@ -0,0 +1,263 @@
+import logging
+import optuna
+
+from copy import deepcopy
+from optuna.samplers import RandomSampler
+from optuna.trial import FixedTrial, BaseTrial
+from optuna.distributions import BaseDistribution
+from optuna import distributions
+from typing import Iterable, Optional, Any
+
+
+class RandomValueTrial(FixedTrial):
+    """A Trial following optuna's API.
+    Does not depend on an optuna.Study and can be used as a standalone object.
+    """
+
+    def __init__(
+            self,
+            number: int = 0,
+            seed: Optional[int] = None,
+            sampler: Optional[RandomSampler] = None
+        ):
+        assert not (seed and sampler), \
+            f"Must provide at most one of (seed={seed}, sampler={sampler})"
+        super().__init__(
+            params=None, 
+            number=number,
+        )
+        self.seed = seed
+        self.sampler = sampler or RandomSampler(self.seed)
+
+    def _suggest(self, name: str, distribution: BaseDistribution) -> Any:
+        """Given a parameter's name and its distribution should return the
+        suggested (sampled) value.
+        (Template method from the Trial super-class).
+
+        Parameters
+        ----------
+        name : str
+            The parameter's name (so we don't suggest different values when the
+            same parameter is sampled more than once).
+        distribution : BaseDistribution
+            The distribution to draw from.
+
+        Returns
+        -------
+        The sampled value.
+        """
+
+        if name in self._distributions:
+            # No need to sample if already suggested.
+            distributions.check_distribution_compatibility(
+                self._distributions[name], distribution,
+            )
+            param_value = self._suggested_params[name]
+
+        else:
+            if self._is_fixed_param(name, distribution):
+                param_value = self.system_attrs["fixed_params"][name]
+            elif distribution.single():
+                param_value = distributions._get_single_value(distribution)
+            else:
+                param_value = self.sampler.sample_independent(
+                    study=None, trial=self,     # type: ignore
+                    param_name=name, param_distribution=distribution,
+                )
+
+        self._suggested_params[name] = param_value
+        self._distributions[name] = distribution
+
+        return self._suggested_params[name]
+
+    def _is_fixed_param(self, name: str, distribution: BaseDistribution) -> bool:
+        """Checks if the given parameter name corresponds to a fixed parameter.
+        This implementation does not depend on an optuna.study.
+
+        Parameters
+        ----------
+        name : str
+        distribution : BaseDistribution
+
+        Returns
+        -------
+        Whether the parameter is a fixed parameter.
+        """
+        system_attrs = self._system_attrs
+        if "fixed_params" not in system_attrs:
+            return False
+
+        if name not in system_attrs["fixed_params"]:
+            return False
+
+        param_value = system_attrs["fixed_params"][name]
+        param_value_in_internal_repr = distribution.to_internal_repr(param_value)
+
+        contained = distribution._contains(param_value_in_internal_repr)
+        if not contained:
+            warnings.warn(
+                f"Fixed parameter '{name}' with value {param_value} is out of range "
+                f"for distribution {distribution}."
+            )
+        return contained
+    
+    
+def suggest_callable_hyperparams(
+        trial: BaseTrial,
+        hyperparameter_space: dict,
+        param_prefix: str = "learner",
+    ) -> dict:
+    """Suggests the top-level hyperparameters for a class instantiation, or
+    for parameterizing any other callable.
+
+    This includes the classpath/importpath, and the conditional
+    hyperparameters to use as kwargs for the class.
+
+    Parameters
+    ----------
+    trial : BaseTrial
+        The trial object to interact with the hyperparameter sampler.
+    hyperparameter_space : dict
+        A dictionary representing a hyperparameter space.
+    param_prefix : str
+        The prefix to attach to all parameters' names in order to uniquely
+        identify them.
+
+    Returns
+    -------
+    Dict
+        The suggested callable + its suggested key-word arguments.
+    """
+
+    # Suggest callable type (top-level callable choice)
+    callable_type = trial.suggest_categorical(
+        f"{param_prefix}_type", list(hyperparameter_space.keys()),
+    )
+
+    hyperparam_subspace = hyperparameter_space[callable_type]
+    callable_classpath = hyperparam_subspace['classpath']
+
+    # Suggest callable kwargs (on conditional sub-space)
+    callable_kwargs = suggest_hyperparams(
+        trial, hyperparam_subspace.get('kwargs', {}),
+        param_prefix=f"{param_prefix}_{callable_type}"
+    )
+
+    hyperparams = {'classpath': callable_classpath, **callable_kwargs}
+    return hyperparams
+
+
+def suggest_hyperparams(
+        trial: BaseTrial,
+        hyperparameter_space: dict,
+        param_prefix: str = '',
+    ) -> dict:
+    """Uses the provided hyperparameter space to suggest specific
+    configurations using the given Trial object.
+
+    Parameters
+    ----------
+    trial : BaseTrial
+        The trial object to interact with the hyperparameter sampler.
+    hyperparameter_space : dict
+        A dictionary representing a hyperparameter space.
+    param_prefix : str
+        The prefix to attach to all parameters' names in order to uniquely
+        identify them.
+
+    Returns
+    -------
+    An instantiation of the given hyperparameter space.
+    """
+    if not hyperparameter_space:
+        logging.warning("Got empty hyperparameter space. Returning '{}'.")
+        return dict()
+    assert isinstance(hyperparameter_space, dict)
+
+    params = dict()
+    for key, value in hyperparameter_space.items():
+        param_id = f'{param_prefix}_{key}'
+
+        # Fixed value
+        if isinstance(value, (str, int, float)) or not isinstance(value, Iterable):
+            params[key] = value
+
+        # Categorical parameter
+        elif isinstance(value, (list, tuple)):
+            # Categorical parameters must be of one of the following types
+            valid_categ_types = (bool, int, float, str)
+
+            # If not, map values to strings for optuna, then remap them back
+            if not all(el is None or isinstance(el, valid_categ_types) for el in value):
+                categ_map = {str(el): el for el in value}
+                suggested_categ_encoded = trial.suggest_categorical(
+                    param_id, sorted(categ_map.keys()),
+                )
+                params[key] = categ_map[str(suggested_categ_encoded)]
+
+            # If categorical values have valid types, use them directly
+            else:
+                params[key] = trial.suggest_categorical(
+                    param_id, value,
+                )
+
+        # Numerical parameter
+        elif isinstance(value, dict) and 'type' in value:
+            params[key] = suggest_numerical_hyperparam(
+                trial, value, param_id,
+            )
+
+        # Nested parameter
+        elif isinstance(value, dict):
+            params[key] = suggest_hyperparams(
+                trial, value, f"{key}_{param_prefix}",
+            )
+
+        else:
+            raise ValueError(
+                f"Invalid hyperparameter configuration: {key}={value} "
+                f"({type(key)} -> {type(value)})."
+            )
+
+    return params
+
+
+def suggest_numerical_hyperparam(
+        trial: BaseTrial,
+        config: dict,
+        param_id: str,
+    ) -> float:
+    """Helper function to suggest a numerical hyperparameter.
+
+    Parameters
+    ----------
+    trial : BaseTrial
+        The trial object to interact with the hyperparameter sampler.
+    config : dict
+        The distribution to sample the parameter from.
+    param_id : str
+        The parameter's name.
+
+    Returns
+    -------
+    The sampled value.
+    """
+    # Parameter's type
+    config = deepcopy(config)
+    param_type = config.pop('type')
+    valid_param_types = ('int', 'float', 'uniform', 'discrete_uniform', 'loguniform')
+    assert param_type in valid_param_types, \
+        f'Invalid parameter type {param_type}, choose one of {valid_param_types}'
+
+    # Parameter's range
+    assert 'range' in config and isinstance(config['range'], (list, tuple)), \
+        'Must provide a range when searching a numerical parameter'
+    low, high = config.pop('range')
+
+    # Remaining suggestion parameters are used as is (e.g., log scale)
+    suggest_param_func = getattr(trial, f'suggest_{param_type}')
+    return suggest_param_func(
+        param_id, low, high, **config,
+    )
+
+
